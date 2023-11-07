fa4150d46c170b77e609b1351e6fe03903372462
Funccclub <43726335+Funccclub@users.noreply.github.com>
2023-11-08 02:26:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ data = {
         'passwd': passwd
 }
 
+content = '未知错误'  # 预先定义 content 变量
+
 try:
     print('进行登录...')
     response = json.loads(session.post(url=login_url,headers=header,data=data).text)
@@ -50,10 +52,11 @@ try:
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
     content = result['msg']
+except Exception as e:
+    content = str(e)
 finally:
     # 无论签到成功还是失败，都发送推送
     print(f'推送内容: {content}')
     if corpid and corpsecret and agentid:
         send_wechat_msg(content, corpid, corpsecret, agentid)
         print('推送成功')
-
